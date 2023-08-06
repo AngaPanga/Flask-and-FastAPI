573e765f793cc61c145412e2f745ae9b580ddc8f
AngaPanga <w13w13@yandex.ru>
2023-08-06 23:03:54 +0300

    дз 2 part 2

--- main.py
+++ main.py
@@ -6,27 +6,34 @@ app.secret_key = b'5f214cacbd30c2ae4784b520f17912ae0d5d8c16ae98128e3f54954622126
 
 @app.get('/')
 def get_index():
-    #if request.cookies.get('Username'):
-    return f"Привет, {request.cookies.get('Username')}" \
-            f"<br>e-mail  -  {request.cookies.get('Email')}"
-    #else:
-    #    return redirect(url_for('login'))
+    if request.cookies.get('Username'):
+        context = {'title': 'Главная',
+                   'name': request.cookies.get('Username'),
+                   'email': request.cookies.get('Email')}
+        return render_template('main.html', **context)
+    else:
+        return redirect(url_for('login'))
 
 
 @app.post('/')
 def post_index():
-    return redirect(url_for('login'))
+    response = redirect(url_for('login'))
+    # пробовал разные варианты, а так же ваш код, но работает только так
+    for el in request.cookies:
+        response.set_cookie(el, expires=0)
+    return response
 
 
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
     context = {'title': 'Вход'}
-    response = make_response(render_template('login.html', **context))
     if request.method == 'POST':
+        response = redirect(url_for('get_index'))
+        response.headers['new_head'] = 'New value'
         response.set_cookie('Username', request.form.get('name'))
         response.set_cookie('Email', request.form.get('mail'))
-        return redirect(url_for('index'))
-    return response
+        return response
+    return render_template('login.html', **context)
 
 
 if __name__ == '__main__':
